81fce30e1b076dc20404cdc06e16a3b9817eb23e
Darpan Chakrabarti <darpanchakrabarti@gmail.com>
2025-07-04 13:58:33 +0530

    Earthquake.py

--- Earthquake.py
+++ Earthquake.py
@@ -5,21 +5,21 @@ from scipy.integrate import solve_ivp
 g = 9.81 # Constants
 L1 = L2 = 1.0
 m1 = m2 = 1.0
-t_max = 30
+t_max = 30 # Time parameters
 dt = 0.05
 t_eval = np.arange(0, t_max, dt)
 
 earthquake_amplitude = 5.0  # (A) m/s² Earth quack parameters
 earthquake_frequency = 1.0  # f (Hz)
 
-def earthquake_acceleration(t):
+def earthquake_acceleration(t): # 
     return earthquake_amplitude * np.sin(2 * np.pi * earthquake_frequency * t)
 
 def deriv(t, y):
     theta1, omega1, theta2, omega2 = y
     delta = theta1 - theta2
     a_t = earthquake_acceleration(t)  # Get current ground acceleration
-    M11 = (m1 + m2) * L1
+    M11 = (m1 + m2) * L1  # Mass matrix
     M12 = m2 * L2 * np.cos(delta)
     M21 = L1 * np.cos(delta)
     M22 = L2
@@ -34,10 +34,11 @@ def deriv(t, y):
     domega1, domega2 = accel
     return omega1, domega1, omega2, domega2
 
-y0_1 = [0, 0, 0, 0]  # Pendulum conditions
-sol1 = solve_ivp(deriv, [0, t_max], y0_1, t_eval=t_eval, rtol=1e-10, atol=1e-10)
+y0_1 = [0, 0, 0, 0] # Initial conditions
 
-def get_xy(sol):
+sol1 = solve_ivp(deriv, [0, t_max], y0_1, t_eval=t_eval, rtol=1e-10, atol=1e-10) # Solve ODEs
+
+def get_xy(sol): # Convert angles to (x, y)
     theta1, theta2 = sol.y[0], sol.y[2]
     x1 = L1 * np.sin(theta1)
     y1 = -L1 * np.cos(theta1)
@@ -46,13 +47,14 @@ def get_xy(sol):
     return x1, y1, x2, y2
 
 x1_1, y1_1, x2_1, y2_1 = get_xy(sol1)
-fig, ax = plt.subplots()
-ax.set_xlim(-3, 3) 
-ax.set_ylim(-3, 3)
-ax.set_aspect('equal')
-ax.set_title("Double Pendulum with Earthquake")
 
-line1, = ax.plot([], [], 'o-', lw=2, label='Pendulum')
+fig, ax = plt.subplots() # Set up animation
+ax.set_xlim(-2.2, 2.2)
+ax.set_ylim(-2.2, 2.2)
+ax.set_aspect('equal')
+ax.set_title("Double Pendulum")
+
+line1, = ax.plot([], [], 'o-', lw=2, label='Pendulum', color = 'red')
 time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
 
 def init():
@@ -61,15 +63,12 @@ def init():
     return line1, time_text
 
 def update(i):
-
-    if i == 500:
-        plt.savefig("D:\Bacchuga\Projects\Images\Figure8f.eps", format='eps', dpi=300)
-
     line1.set_data([0, x1_1[i], x2_1[i]], [0, y1_1[i], y2_1[i]])
     time_text.set_text(f'Time = {i * dt:.1f}s')
     return line1, time_text
 
 from matplotlib.animation import FuncAnimation
+
 ani = FuncAnimation(fig, update, frames=len(t_eval), init_func=init,
                     blit=True, interval=dt * 1000)
 
